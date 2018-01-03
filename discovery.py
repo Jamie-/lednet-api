b1145969e079afd5e0174243e5aa254e86038330
Jamie Scott <github@jami.org.uk>
2018-01-03 21:48:20 +0000

    Updated intelligent discovery

--- discovery.py
+++ discovery.py
@@ -1,8 +1,9 @@
 from threading import Thread, Event
 import socket
+import json
 
 PORT = 50000 # Server port
-KEY = "a827fgh24fg" # Validity key
+INDICATOR = "LEDnet" # Indicator packet is for LEDnet
 
 _stop_event = Event()
 
@@ -10,16 +11,19 @@ def discover():
     print "[INFO] Starting discovery thread..."
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create UDP socket
     s.bind(('', PORT)) # Bind to port
-    my_ip = socket.gethostbyname(socket.gethostname()) # Get server IP
+
+    d = {} # Data to send back to client upon searching for server
+    d['hostname'] = socket.gethostname() # Get this hostname
+    d['ip'] = socket.gethostbyname(d['hostname']) # Get this IP
     s.settimeout(1.0)
 
     # Handle incoming broadcasts
     while not is_stopped():
         try:
             data, addr = s.recvfrom(1024) # Wait for a packet
-            if data == KEY:
+            if data == INDICATOR:
                 print "[INFO] Discovery request from {}:{}".format(addr[0], addr[1])
-                s.sendto(KEY+my_ip, (addr[0], addr[1]))
+                s.sendto(json.dumps(d), (addr[0], addr[1]))
         except socket.timeout as e:
             pass
 
